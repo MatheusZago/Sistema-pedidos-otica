ca575f55cd218da477059d53735bd6b0ce5b92da
Matheus <matheuszago134@gmail.com>
2026-02-12 14:08:29 -0300

    Add local variables

---
+++ backend/scripts/run-dev.bat
@@ -0,0 +1,14 @@
+@echo off
+
+echo ================================
+echo Iniciando Sistema de Vendas
+echo ================================
+
+REM Mude as variaveis de acordo com seu banco de dados.
+set DB_URL=jdbc:postgresql://localhost:5432/pedidos_otica
+set DB_USERNAME=postgres
+set DB_PASSWORD=postgres
+
+mvn spring-boot:run
+
+pause
