b95bd130680f8a6bafe0438cacfd0c4c30e2c31a
Matheus <matheuszago134@gmail.com>
2026-05-05 13:47:19 -0300

    Add base configuration

---
+++ backend/src/main/java/com/matheusluizago/backend/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.matheusluizago.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                ).build();
+    }
+}
